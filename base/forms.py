0aa238f3a0a6fd4d81fb6ecb738668de1d8cc79b
waisalkindy <waisalkindy@gmail.com>
2024-04-23 14:00:00 +0100

    create forms

--- base/forms.py
+++ base/forms.py
@@ -7,4 +7,4 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password4', 'password3']
